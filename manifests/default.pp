b634203dbc93a7de7649b4d6590480e7681159e4
Noah Murphy <badgerark@gmail.com>
2014-10-30 15:56:23 -0700

    start the manifest

---
+++ manifests/default.pp
@@ -0,0 +1,21 @@
+exec { "apt-get update":
+  ``path => "/usr/bin",
+  }
+  package { "nginx":
+    ensure  => present,
+  ``require => Exec["apt-get update"],
+      }
+      package { "rails":
+        ensure => present,
+          provider => gem,
+          }
+  service { "ngnix":
+    ensure  => "running",
+    require => Package["ngnix"],
+          }
+  file { "/var/www/sample-webapp":
+    ensure  => "link",
+    target  => "/vagrant/sample-webapp",
+    require => Package["ngnix"],
+    notify  => Service["ngnix"],
+  }
